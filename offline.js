3d61e785b491cdbca1c9115416cad31bbb450358
Indri Junanda <indrijunanda26@gmail.com>
2020-09-08 16:42:30 +0700

    initial commit

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1512835238,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/kartu-sheet0.png",
+		"images/kartu-sheet1.png",
+		"images/kartu-sheet2.png",
+		"images/timer.png",
+		"images/textgameover.png",
+		"images/background-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
